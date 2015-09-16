e710143ce7e0e048b352613e59e51689667682d3
angelocardona85 <angelocardona85@gmail.com>
2015-09-16 17:02:59 -0500

    creacion clase entidad solicitudes

---
+++ WebServicesDemoMovil/Entidades/Solicitudes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServicesDemoMovil.Entidades
+{
+    public class Solicitudes
+    {
+        public int Id { get; set; }
+        public string ImagenUrl { get; set; }
+        public string NombreImagen { get; set; }
+        public string Solicitud { get; set; }
+        public int AreaSolicitud { get; set; }
+        public bool Activa { get; set; }
+        public bool Atendido { get; set; }
+        public int AtendidoPor { get; set; }
+        public int SolicitadoPor { get; set; }
+        public DateTime FechaSolicitud { get; set; }
+        public DateTime FechaAtencion { get; set; }
+        public byte[] ImagenBytes { get; set; }
+    }
+}
